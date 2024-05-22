fac2a197f1a20c6e4f18879efd67befdb04e26db
Arthur Silva <arthur.santos.off@gmail.com>
2024-05-21 21:43:46 -0300

    feat: added createQuestion route with authentication and body validation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,16 +1,55 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Controller, Post, UseGuards, Body } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { UserPayload } from 'src/auth/jwt-strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipes'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller('/questions')
 @UseGuards(AuthGuard('jwt'))
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user)
-    return 'hello jwt'
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(bodyValidationPipe)
+    body: CreateQuestionBodySchema,
+  ) {
+    const userId = user.sub
+
+    const { title, content } = body
+    const { slug } = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorID: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  // used to create a better slug with no accents and white spaces
+  private convertToSlug(title: string): { slug: string } {
+    return {
+      slug: title
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[^\w\s-]/g, '')
+        .replace(/\s+/g, '-'),
+    }
   }
 }
