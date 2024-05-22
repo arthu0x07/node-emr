7868eb46074154e7ce0500c38f801bc273bbcc11
Arthur Silva <arthur.santos.off@gmail.com>
2024-05-21 22:21:54 -0300

    chore: moved JwtModule registration to a variable for cleaner code structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,24 +5,23 @@ import { ConfigService } from '@nestjs/config'
 import { Env } from '@/env'
 import { JwtStrategy } from './jwt-strategy'
 
-@Module({
-  imports: [
-    PassportModule,
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+const JwtRegister = JwtModule.registerAsync({
+  inject: [ConfigService],
+  global: true,
+  useFactory(config: ConfigService<Env, true>) {
+    const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
 
-        return {
-          signOptions: { algorithm: 'RS256' },
-          privateKey: Buffer.from(privateKey, 'base64'),
-          publicKey: Buffer.from(publicKey, 'base64'),
-        }
-      },
-    }),
-  ],
+    return {
+      signOptions: { algorithm: 'RS256' },
+      privateKey: Buffer.from(privateKey, 'base64'),
+      publicKey: Buffer.from(publicKey, 'base64'),
+    }
+  },
+})
+
+@Module({
+  imports: [PassportModule, JwtRegister],
   providers: [JwtStrategy],
 })
 export class AuthModule {}
